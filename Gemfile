a535fad45e3ab40f3c6d37362bc4ac66d5d83b9c
pedro <701.pedro.ferreira@gmail.com>
2024-09-02 19:26:04 +0100

    user authentication added

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ group :test do
 end
 
 gem 'cssbundling-rails'
+gem "devise", "~> 4.9"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+    devise_scope :user do
+    get "/users", to: "devise/registrations#new"
+    get "/users/password", to: "devise/passwords#new"
+    get "/users/sign_out" => "devise/sessions#destroy"
+   end
+
+  devise_for :users
+  get "home/about"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,6 +17,11 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+
+
+
+
 
   # Defines the root path route ("/")
   root "posts#index"
